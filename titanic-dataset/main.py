2dd256b811289c081e84e2f268d89b329fd01924
Bertram Chabu <145387124+bertramchabu@users.noreply.github.com>
2025-12-11 23:09:06 +0300

    Update main.py

--- titanic-dataset/main.py
+++ titanic-dataset/main.py
@@ -7,3 +7,27 @@ df = pd.read_csv('Titanic-Dataset.csv')
 info = df.info()
 # columns for our dataset
 titles = df.head()
+
+print("checking for duplicates\n\n")
+
+duplicates = df.duplicated()
+print(duplicates)
+
+
+print("identifying column Data types")
+category_columns = [col for col in df.columns if df[col].dtype == 'object']
+numerical_columns  = [col for col in df.columns if df[col].dtypes != 'object']
+
+print('Categorical clolumns: ', category_columns)
+print('Numerical columns: ', numerical_columns)
+
+
+print('Identifying unique values in Columns\n\n\n')
+
+
+unique_values = df[category_columns].nunique()
+print(unique_values)
+
+print("Calculate missing Values as Percentage")
+missing_values_percentage  = round((df.isnull().sum() / df.shape[0]) * 100, 2)
+print(missing_values_percentage)
