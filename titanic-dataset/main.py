90f319f1fee77af91e66076cdb6e2e5412359a8a
Bertram Chabu <145387124+bertramchabu@users.noreply.github.com>
2025-12-11 22:58:33 +0300

    Update main.py

--- titanic-dataset/main.py
+++ titanic-dataset/main.py
@@ -1,3 +1,9 @@
 import pandas as pd
-import numpy as np
+# import numpy as np
 
+# create a new dataframe 
+df = pd.read_csv('Titanic-Dataset.csv')
+# information about our dataset
+info = df.info()
+# columns for our dataset
+titles = df.head()
